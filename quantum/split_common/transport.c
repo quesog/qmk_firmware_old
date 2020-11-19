techn0punk/qmk_firmware

Add rgb matrix split code

quantum/split_common/transport.c
@@ -17,6 +17,10 @@ extern keyboard_state_t keyboard_state;
 #    include "rgblight.h"
 #endif
 
+#ifdef RGB_MATRIX_ENABLE
+#    include "rgb_matrix.h"
+#endif
+
 #ifdef BACKLIGHT_ENABLE
 #    include "backlight.h"
 #endif
@@ -26,6 +30,10 @@ extern keyboard_state_t keyboard_state;
 static pin_t encoders_pad[] = ENCODERS_PAD_A;
 #    define NUMBER_OF_ENCODERS (sizeof(encoders_pad) / sizeof(pin_t))
 #endif
+
+#define RGB_SHOULD_SPLIT_MATRIX (defined(RGB_MATRIX_SPLIT) && defined(RGB_MATRIX_ENABLE))
+#define RGB_SHOULD_SPLIT_LIGHT (defined(RGBLIGHT_SPLIT) && defined(RGBLIGHT_ENABLE))
+#define RGB_SHOULD_SPLIT (RGB_SHOULD_SPLIT_MATRIX || RGB_SHOULD_SPLIT_LIGHT)
 
 #if defined(USE_I2C)
 
@@ -136,9 +144,9 @@ void transport_slave_init(void) { i2c_slave_init(SLAVE_I2C_ADDRESS); }
 
 extern mailbox_t transaction_received_mailbox;
 void             react_received_primary(matrix_row_t matrix[]);
-void             react_received_secondary(void);
+void             react_received_secondary(matrix_row_t matrix[]);
 void             transport_secondary(matrix_row_t matrix[]);
-bool             transport_primary(void);
+bool             transport_primary(matrix_row_t matrix[]);
 
 typedef struct _Serial_s2m_buffer_t {
     // TODO: if MATRIX_COLS > 8 change to uint8_t packed_matrix[] for pack/unpack
@@ -151,15 +159,16 @@ typedef struct _Serial_s2m_buffer_t {
 } Serial_s2m_buffer_t;
 
 typedef struct _Serial_m2s_buffer_t {
+    uint32_t sync_timer;
 #    ifdef BACKLIGHT_ENABLE
-    uint8_t backlight_level;
+    uint8_t  backlight_level;
 #    endif
 #    ifdef WPM_ENABLE
-    uint8_t current_wpm;
+    uint8_t  current_wpm;
 #    endif
 } Serial_m2s_buffer_t;
 
-#    if defined(RGBLIGHT_ENABLE) && defined(RGBLIGHT_SPLIT)
+#    if RGB_SHOULD_SPLIT
 // When MCUs on both sides drive their respective RGB LED chains,
 // it is necessary to synchronize, so it is necessary to communicate RGB
 // information. In that case, define RGBLIGHT_SPLIT with info on the number
@@ -168,12 +177,16 @@ typedef struct _Serial_m2s_buffer_t {
 // Otherwise, if the master side MCU drives both sides RGB LED chains,
 // there is no need to communicate.
 
-typedef struct _Serial_rgblight_t {
+typedef struct _Serial_rgb_t {
+#        if defined(RGB_MATRIX_ENABLE)
+    rgb_matrix_syncinfo_t rgb_matrix_sync;
+#        elif defined(RGBLIGHT_ENABLE)
     rgblight_syncinfo_t rgblight_sync;
-} Serial_rgblight_t;
+#        endif
+} Serial_rgb_t;
 
-volatile Serial_rgblight_t serial_rgblight = {};
-uint8_t volatile status_rgblight           = 0;
+volatile Serial_rgb_t serial_rgb = {};
+uint8_t volatile status_rgb      = 0;
 #    endif
 
 volatile Serial_s2m_buffer_t serial_s2m_buffer = {};
@@ -183,8 +196,9 @@ uint8_t volatile status0                       = 0;
 enum serial_transaction_id {
     SEND_METADATA_TO_SECONDARY = 0,
     SEND_MATRIX_TO_PRIMARY,
-#    if defined(RGBLIGHT_ENABLE) && defined(RGBLIGHT_SPLIT)
-    PUT_RGBLIGHT,
+    SEND_MATRIX_TO_SECONDARY,
+#    if RGB_SHOULD_SPLIT
+    PUT_RGB,
 #    endif
 };
 
@@ -206,10 +220,18 @@ SSTD_t transactions[] = {
             0,
             NULL,
         },
-#    if defined(RGBLIGHT_ENABLE) && defined(RGBLIGHT_SPLIT)
-    [PUT_RGBLIGHT] =
+    [SEND_MATRIX_TO_SECONDARY] =
         {
-            (uint8_t *)&status_rgblight, sizeof(serial_rgblight), (uint8_t *)&serial_rgblight, 0, NULL  // no slave to master transfer
+            (uint8_t *)&status0,
+            sizeof(serial_s2m_buffer),
+            (uint8_t *)&serial_s2m_buffer,
+            0,
+            NULL,
+        },
+#    if RGB_SHOULD_SPLIT
+    [PUT_RGB] =
+        {
+            (uint8_t *)&status_rgb, sizeof(serial_rgb), (uint8_t *)&serial_rgb, 0, NULL  // no slave to master transfer
         },
 #    endif
 };
@@ -217,36 +239,44 @@ SSTD_t transactions[] = {
 void transport_master_init(void) { serial_init(transactions, TID_LIMIT(transactions)); }
 void transport_slave_init(void) { serial_init(transactions, TID_LIMIT(transactions)); }
 
-#    if defined(RGBLIGHT_ENABLE) && defined(RGBLIGHT_SPLIT)
+#    if RGB_SHOULD_SPLIT
 
 // rgblight synchronization information communication.
 
-void transport_rgblight_primary(void) {
+void transport_rgb_primary(void) {
     if (rgblight_get_change_flags()) {
-        rgblight_get_syncinfo((rgblight_syncinfo_t *)&serial_rgblight.rgblight_sync);
-        if (serial_transaction(PUT_RGBLIGHT) == TRANSACTION_END) {
+#        ifdef RGB_MATRIX_ENABLE
+        rgb_matrix_get_syncinfo((rgb_matrix_syncinfo_t *)&serial_rgb.rgb_matrix_sync);
+#        elif defined(RGBLIGHT_ENABLE)
+        rgblight_get_syncinfo((rgblight_syncinfo_t *)&serial_rgb.rgblight_sync);
+#        endif
+        if (serial_transaction(PUT_RGB) == TRANSACTION_END) {
             rgblight_clear_change_flags();
         }
     }
 }
 
-void transport_rgblight_secondary(void) {
-    if (status_rgblight == TRANSACTION_ACCEPTED) {
-        rgblight_update_sync((rgblight_syncinfo_t *)&serial_rgblight.rgblight_sync, false);
-        status_rgblight = TRANSACTION_END;
+void transport_rgb_secondary(void) {
+    if (status_rgb == TRANSACTION_ACCEPTED) {
+#        if defined(RGB_MATRIX_ENABLE)
+        rgb_matrix_update_sync((rgb_matrix_syncinfo_t *)&serial_rgb.rgb_matrix_sync);
+#        elif defined(RGBLIGHT_ENABLE)
+        rgblight_update_sync((rgblight_syncinfo_t *)&serial_rgb.rgblight_sync, false);
+#        endif
+        status_rgb = TRANSACTION_END;
     }
 }
 
 #    else
-#        define transport_rgblight_primary()
-#        define transport_rgblight_secondary()
+#        define transport_rgb_primary()
+#        define transport_rgb_secondary()
 #    endif
 
 void transport_task(void) {
     if (is_keyboard_master()) {
         static uint8_t error_count;
-        transport_rgblight_primary();
-        if (!transport_primary()) {
+        transport_rgb_primary();
+        if (!transport_primary(matrix + thisHand)) {
             error_count++;
             // TODO ERROR HANDLING DOES NOTHING ATM
             if (error_count > ERROR_DISCONNECT_COUNT) {
@@ -260,28 +290,62 @@ void transport_task(void) {
         }
         react_received_primary(matrix + thatHand);
     } else {
-        transport_rgblight_secondary();
+        transport_rgb_secondary();
         transport_secondary(matrix + thisHand);
-        react_received_secondary();
+        react_received_secondary(matrix + thatHand);
     }
 }
 
-bool transport_primary(void) {
+bool transport_primary(matrix_row_t matrix[]) {
+#    ifdef SPLIT_TRANSPORT_MIRROR
+    if (keyboard_state.matrix_changed || keyboard_state.encoder_changed) {
+        chMtxLock(&transactions_mutex);
+        // TODO: if MATRIX_COLS > 8 change to pack()
+        for (int i = 0; i < ROWS_PER_HAND; ++i) {
+            serial_s2m_buffer.smatrix[i] = matrix[i];
+        }
+#        ifdef ENCODER_ENABLE
+        //  if (keyboard_state.encoder_changed) {
+        encoder_state_raw((uint8_t *)serial_s2m_buffer.encoder_state);
+        //   }
+#        endif
+        chMtxUnlock(&transactions_mutex);
+        if (serial_transaction(SEND_MATRIX_TO_SECONDARY) != TRANSACTION_END) {
+            // return;
+        }
+    }
+#    endif
+
     chMtxLock(&transactions_mutex);
 
     bool           send_meta           = false;
 #    ifdef BACKLIGHT_ENABLE
     static uint8_t backlight_level_old = 0;
     serial_m2s_buffer.backlight_level  = is_backlight_enabled() ? get_backlight_level() : 0;
-    send_meta                          = backlight_level_old != serial_m2s_buffer.backlight_level;
+    if (backlight_level_old != serial_m2s_buffer.backlight_level) {
+        send_meta          = true;
+        backligt_level_old = serial_m2s_buffer.backlight_level;
+    }
+
 #    endif
 
 #    ifdef WPM_ENABLE
-    static uint8_t wpm_old             = 0;
-    serial_m2s_buffer.current_wpm      = get_current_wpm();
-    send_meta                          = send_meta || wpm_old != serial_m2s_buffer.current_wpm;
+    static uint8_t wpm_old        = 0;
+    serial_m2s_buffer.current_wpm = get_current_wpm();
+    if (wpm_old != serial_m2s_buffer.current_wpm) {
+        send_meta = true;
+        wpm_old   = serial_m2s_buffer.current_wpm;
+    }
+
 #    endif
 
+    sync_timer_update(timer_read32());
+
+    uint32_t sync_timer_new = sync_timer_read32();
+    if ((serial_m2s_buffer.sync_timer - sync_timer_new) > 1000) {
+        serial_m2s_buffer.sync_timer = sync_timer_new;
+        send_meta                    = true;
+    }
     chMtxUnlock(&transactions_mutex);
 
     if (send_meta) {
@@ -337,7 +401,7 @@ void react_received_primary(matrix_row_t matrix[]) {
     }
 }
 
-void react_received_secondary(void) {
+void react_received_secondary(matrix_row_t matrix[]) {
     uint8_t sstd_index;
     chSysLock();
     msg_t msg = chMBFetchI(&transaction_received_mailbox, (msg_t *)(&sstd_index));
@@ -350,6 +414,8 @@ void react_received_secondary(void) {
     if (sstd_index == SEND_METADATA_TO_SECONDARY) {
         chMtxLock(&transactions_mutex);
 
+        sync_timer_update(serial_m2s_buffer.sync_timer);  // todo offset calculation
+
 #    ifdef WPM_ENABLE
         set_current_wpm(serial_m2s_buffer.current_wpm);
 #    endif
@@ -359,6 +425,25 @@ void react_received_secondary(void) {
 #    endif
         chMtxUnlock(&transactions_mutex);
     }
+
+#    ifdef SPLIT_TRANSPORT_MIRROR
+    if (sstd_index == SEND_MATRIX_TO_SECONDARY) {
+        chMtxLock(&transactions_mutex);
+
+        // TODO:  if MATRIX_COLS > 8 change to unpack()
+        for (int i = 0; i < ROWS_PER_HAND; ++i) {
+            matrix[i] = serial_s2m_buffer.smatrix[i];
+        }
+#        ifdef ENCODER_ENABLE
+        encoder_update_raw((uint8_t *)serial_s2m_buffer.encoder_state);
+#        endif
+        chMtxUnlock(&transactions_mutex);
+    }
+#    endif
 }
+
+#    undef RGB_SHOULD_SPLIT
+#    undef RGB_SHOULD_SPLIT_MATRIX
+#    undef RGB_SHOULD_SPLIT_LIGHT
 
 #endif

quesog/qmk_firmware_old

quantum/split_common/transport.c
@@ -17,6 +17,10 @@ extern keyboard_state_t keyboard_state;
 #    include "rgblight.h"
 #endif
 
+#ifdef RGB_MATRIX_ENABLE
+#    include "rgb_matrix.h"
+#endif
+
 #ifdef BACKLIGHT_ENABLE
 #    include "backlight.h"
 #endif
@@ -26,6 +30,10 @@ extern keyboard_state_t keyboard_state;
 static pin_t encoders_pad[] = ENCODERS_PAD_A;
 #    define NUMBER_OF_ENCODERS (sizeof(encoders_pad) / sizeof(pin_t))
 #endif
+
+#define RGB_SHOULD_SPLIT_MATRIX (defined(RGB_MATRIX_SPLIT) && defined(RGB_MATRIX_ENABLE))
+#define RGB_SHOULD_SPLIT_LIGHT (defined(RGBLIGHT_SPLIT) && defined(RGBLIGHT_ENABLE))
+#define RGB_SHOULD_SPLIT (RGB_SHOULD_SPLIT_MATRIX || RGB_SHOULD_SPLIT_LIGHT)
 
 #if defined(USE_I2C)
 
@@ -136,9 +144,9 @@ void transport_slave_init(void) { i2c_slave_init(SLAVE_I2C_ADDRESS); }
 
 extern mailbox_t transaction_received_mailbox;
 void             react_received_primary(matrix_row_t matrix[]);
-void             react_received_secondary(void);
+void             react_received_secondary(matrix_row_t matrix[]);
 void             transport_secondary(matrix_row_t matrix[]);
-bool             transport_primary(void);
+bool             transport_primary(matrix_row_t matrix[]);
 
 typedef struct _Serial_s2m_buffer_t {
     // TODO: if MATRIX_COLS > 8 change to uint8_t packed_matrix[] for pack/unpack
@@ -151,15 +159,16 @@ typedef struct _Serial_s2m_buffer_t {
 } Serial_s2m_buffer_t;
 
 typedef struct _Serial_m2s_buffer_t {
+    uint32_t sync_timer;
 #    ifdef BACKLIGHT_ENABLE
-    uint8_t backlight_level;
+    uint8_t  backlight_level;
 #    endif
 #    ifdef WPM_ENABLE
-    uint8_t current_wpm;
+    uint8_t  current_wpm;
 #    endif
 } Serial_m2s_buffer_t;
 
-#    if defined(RGBLIGHT_ENABLE) && defined(RGBLIGHT_SPLIT)
+#    if RGB_SHOULD_SPLIT
 // When MCUs on both sides drive their respective RGB LED chains,
 // it is necessary to synchronize, so it is necessary to communicate RGB
 // information. In that case, define RGBLIGHT_SPLIT with info on the number
@@ -168,12 +177,16 @@ typedef struct _Serial_m2s_buffer_t {
 // Otherwise, if the master side MCU drives both sides RGB LED chains,
 // there is no need to communicate.
 
-typedef struct _Serial_rgblight_t {
+typedef struct _Serial_rgb_t {
+#        if defined(RGB_MATRIX_ENABLE)
+    rgb_matrix_syncinfo_t rgb_matrix_sync;
+#        elif defined(RGBLIGHT_ENABLE)
     rgblight_syncinfo_t rgblight_sync;
-} Serial_rgblight_t;
+#        endif
+} Serial_rgb_t;
 
-volatile Serial_rgblight_t serial_rgblight = {};
-uint8_t volatile status_rgblight           = 0;
+volatile Serial_rgb_t serial_rgb = {};
+uint8_t volatile status_rgb      = 0;
 #    endif
 
 volatile Serial_s2m_buffer_t serial_s2m_buffer = {};
@@ -183,8 +196,9 @@ uint8_t volatile status0                       = 0;
 enum serial_transaction_id {
     SEND_METADATA_TO_SECONDARY = 0,
     SEND_MATRIX_TO_PRIMARY,
-#    if defined(RGBLIGHT_ENABLE) && defined(RGBLIGHT_SPLIT)
-    PUT_RGBLIGHT,
+    SEND_MATRIX_TO_SECONDARY,
+#    if RGB_SHOULD_SPLIT
+    PUT_RGB,
 #    endif
 };
 
@@ -206,10 +220,18 @@ SSTD_t transactions[] = {
             0,
             NULL,
         },
-#    if defined(RGBLIGHT_ENABLE) && defined(RGBLIGHT_SPLIT)
-    [PUT_RGBLIGHT] =
+    [SEND_MATRIX_TO_SECONDARY] =
         {
-            (uint8_t *)&status_rgblight, sizeof(serial_rgblight), (uint8_t *)&serial_rgblight, 0, NULL  // no slave to master transfer
+            (uint8_t *)&status0,
+            sizeof(serial_s2m_buffer),
+            (uint8_t *)&serial_s2m_buffer,
+            0,
+            NULL,
+        },
+#    if RGB_SHOULD_SPLIT
+    [PUT_RGB] =
+        {
+            (uint8_t *)&status_rgb, sizeof(serial_rgb), (uint8_t *)&serial_rgb, 0, NULL  // no slave to master transfer
         },
 #    endif
 };
@@ -218,36 +240,44 @@ SSTD_t transactions[] = {
 void transport_master_init(void) { soft_serial_initiator_init(transactions, TID_LIMIT(transactions)); }
 void transport_slave_init(void) { soft_serial_target_init(transactions, TID_LIMIT(transactions)); }
 
-#    if defined(RGBLIGHT_ENABLE) && defined(RGBLIGHT_SPLIT)
+#    if RGB_SHOULD_SPLIT
 
 // rgblight synchronization information communication.
 
-void transport_rgblight_primary(void) {
+void transport_rgb_primary(void) {
     if (rgblight_get_change_flags()) {
-        rgblight_get_syncinfo((rgblight_syncinfo_t *)&serial_rgblight.rgblight_sync);
-        if (soft_serial_transaction(PUT_RGBLIGHT) == TRANSACTION_END) {
+#        ifdef RGB_MATRIX_ENABLE
+        rgb_matrix_get_syncinfo((rgb_matrix_syncinfo_t *)&serial_rgb.rgb_matrix_sync);
+#        elif defined(RGBLIGHT_ENABLE)
+        rgblight_get_syncinfo((rgblight_syncinfo_t *)&serial_rgb.rgblight_sync);
+#        endif
+        if (soft_serial_transaction(PUT_RGB) == TRANSACTION_END) {
             rgblight_clear_change_flags();
         }
     }
 }
 
-void transport_rgblight_secondary(void) {
-    if (status_rgblight == TRANSACTION_ACCEPTED) {
-        rgblight_update_sync((rgblight_syncinfo_t *)&serial_rgblight.rgblight_sync, false);
-        status_rgblight = TRANSACTION_END;
+void transport_rgb_secondary(void) {
+    if (status_rgb == TRANSACTION_ACCEPTED) {
+#        if defined(RGB_MATRIX_ENABLE)
+        rgb_matrix_update_sync((rgb_matrix_syncinfo_t *)&serial_rgb.rgb_matrix_sync);
+#        elif defined(RGBLIGHT_ENABLE)
+        rgblight_update_sync((rgblight_syncinfo_t *)&serial_rgb.rgblight_sync, false);
+#        endif
+        status_rgb = TRANSACTION_END;
     }
 }
 
 #    else
-#        define transport_rgblight_primary()
-#        define transport_rgblight_secondary()
+#        define transport_rgb_primary()
+#        define transport_rgb_secondary()
 #    endif
 
 void transport_task(void) {
     if (is_keyboard_master()) {
         static uint8_t error_count;
-        transport_rgblight_primary();
-        if (!transport_primary()) {
+        transport_rgb_primary();
+        if (!transport_primary(matrix + thisHand)) {
             error_count++;
             // TODO ERROR HANDLING DOES NOTHING ATM
             if (error_count > ERROR_DISCONNECT_COUNT) {
@@ -261,28 +291,62 @@ void transport_task(void) {
         }
         react_received_primary(matrix + thatHand);
     } else {
-        transport_rgblight_secondary();
+        transport_rgb_secondary();
         transport_secondary(matrix + thisHand);
-        react_received_secondary();
+        react_received_secondary(matrix + thatHand);
     }
 }
 
-bool transport_primary(void) {
+bool transport_primary(matrix_row_t matrix[]) {
+#    ifdef SPLIT_TRANSPORT_MIRROR
+    if (keyboard_state.matrix_changed || keyboard_state.encoder_changed) {
+        chMtxLock(&transactions_mutex);
+        // TODO: if MATRIX_COLS > 8 change to pack()
+        for (int i = 0; i < ROWS_PER_HAND; ++i) {
+            serial_s2m_buffer.smatrix[i] = matrix[i];
+        }
+#        ifdef ENCODER_ENABLE
+        //  if (keyboard_state.encoder_changed) {
+        encoder_state_raw((uint8_t *)serial_s2m_buffer.encoder_state);
+        //   }
+#        endif
+        chMtxUnlock(&transactions_mutex);
+        if (serial_transaction(SEND_MATRIX_TO_SECONDARY) != TRANSACTION_END) {
+            // return;
+        }
+    }
+#    endif
+
     chMtxLock(&transactions_mutex);
 
     bool           send_meta           = false;
 #    ifdef BACKLIGHT_ENABLE
     static uint8_t backlight_level_old = 0;
     serial_m2s_buffer.backlight_level  = is_backlight_enabled() ? get_backlight_level() : 0;
-    send_meta                          = backlight_level_old != serial_m2s_buffer.backlight_level;
+    if (backlight_level_old != serial_m2s_buffer.backlight_level) {
+        send_meta          = true;
+        backligt_level_old = serial_m2s_buffer.backlight_level;
+    }
+
 #    endif
 
 #    ifdef WPM_ENABLE
-    static uint8_t wpm_old             = 0;
-    serial_m2s_buffer.current_wpm      = get_current_wpm();
-    send_meta                          = send_meta || wpm_old != serial_m2s_buffer.current_wpm;
+    static uint8_t wpm_old        = 0;
+    serial_m2s_buffer.current_wpm = get_current_wpm();
+    if (wpm_old != serial_m2s_buffer.current_wpm) {
+        send_meta = true;
+        wpm_old   = serial_m2s_buffer.current_wpm;
+    }
+
 #    endif
 
+    sync_timer_update(timer_read32());
+
+    uint32_t sync_timer_new = sync_timer_read32();
+    if ((serial_m2s_buffer.sync_timer - sync_timer_new) > 1000) {
+        serial_m2s_buffer.sync_timer = sync_timer_new;
+        send_meta                    = true;
+    }
     chMtxUnlock(&transactions_mutex);
 
     if (send_meta) {
@@ -338,7 +402,7 @@ void react_received_primary(matrix_row_t matrix[]) {
     }
 }
 
-void react_received_secondary(void) {
+void react_received_secondary(matrix_row_t matrix[]) {
     uint8_t sstd_index;
     chSysLock();
     msg_t msg = chMBFetchI(&transaction_received_mailbox, (msg_t *)(&sstd_index));
@@ -351,6 +415,8 @@ void react_received_secondary(void) {
     if (sstd_index == SEND_METADATA_TO_SECONDARY) {
         chMtxLock(&transactions_mutex);
 
+        sync_timer_update(serial_m2s_buffer.sync_timer);  // todo offset calculation
+
 #    ifdef WPM_ENABLE
         set_current_wpm(serial_m2s_buffer.current_wpm);
 #    endif
@@ -360,6 +426,25 @@ void react_received_secondary(void) {
 #    endif
         chMtxUnlock(&transactions_mutex);
     }
+
+#    ifdef SPLIT_TRANSPORT_MIRROR
+    if (sstd_index == SEND_MATRIX_TO_SECONDARY) {
+        chMtxLock(&transactions_mutex);
+
+        // TODO:  if MATRIX_COLS > 8 change to unpack()
+        for (int i = 0; i < ROWS_PER_HAND; ++i) {
+            matrix[i] = serial_s2m_buffer.smatrix[i];
+        }
+#        ifdef ENCODER_ENABLE
+        encoder_update_raw((uint8_t *)serial_s2m_buffer.encoder_state);
+#        endif
+        chMtxUnlock(&transactions_mutex);
+    }
+#    endif
 }
+
+#    undef RGB_SHOULD_SPLIT
+#    undef RGB_SHOULD_SPLIT_MATRIX
+#    undef RGB_SHOULD_SPLIT_LIGHT
 
 #endif