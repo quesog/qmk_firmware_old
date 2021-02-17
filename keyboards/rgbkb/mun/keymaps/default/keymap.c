5cd04633a700afd6183d30f86e69452310c15e34
Ryan Caltabiano <rcalt2vt@gmail.com>
2021-02-16 23:03:52 -0600

    RGBKB Mun Keyboard
    Enabling encoders by default
    
    Updates to Mun
    
    Keymap, config, and RGB layout updates.
    
    Mun Keymap & Encoder updates

---
+++ keyboards/rgbkb/mun/keymaps/default/keymap.c
@@ -0,0 +1,209 @@
+#include QMK_KEYBOARD_H
+
+enum mun_layers {
+    _QWERTY,
+    _COLEMAK,
+    _DVORAK,
+
+    _FN,
+    _ADJUST
+};
+
+enum mun_keycodes {
+    QWERTY = SAFE_RANGE,
+    COLEMAK,
+    DVORAK
+};
+
+#define FN      MO(_FN)
+#define ADJUST  MO(_ADJUST)
+
+#define FN_CAPS  LT(_FN, KC_CAPS)
+#define FN_ESC   LT(_FN, KC_ESC)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* QWERTY
+     * .--------------------------------------------------------------.  .--------------------------------------------------------------.
+     * | `~/ESC | 1      | 2      | 3      | 4      | 5      |   -    |  |    =   | 6      | 7      | 8      | 9      | 0      | Bckspc |
+     * |--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+     * | Tab    | Q      | W      | E      | R      | T      |   [    |  |    ]   | Y      | U      | I      | O      | P      | \      |
+     * |--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+     * | FN/Caps| A      | S      | D      | F      | G      |   (    |  |    )   | H      | J      | K      | L      | :      | '      |
+     * |--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+     * | Shift( | Z      | X      | C      | V      | B      |   {    |  |    }   | N      | M      | ,      | .      | /      | )Shift |
+     * |--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+     * | Ctrl   | Win    | Alt    | RGBTOG | FN     | Space  | Bksp   |  | Enter  | Space  | Space  | FN     | Alt    | Win    | Ctrl   |
+     * '--------+--------+--------+--------+--------+--------+--------'  '--------+--------+--------+--------+--------+--------+--------'
+     */
+    [_QWERTY] = LAYOUT(
+        KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS,   KC_EQL,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,   KC_RBRC,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_PIPE,
+        FN_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LPRN,   KC_RPRN,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LCBR,   KC_RCBR,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
+        KC_LCTL, KC_LGUI, KC_LALT, RGB_TOG, ADJUST,  KC_SPC,  KC_DEL,    KC_ENT,    KC_SPC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_RCTL
+    ),
+
+    [_COLEMAK] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    _______, _______, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, _______,
+        _______, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    _______, _______, KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    _______,
+        _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, _______, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    ),
+
+    [_DVORAK] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    _______, _______, KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    _______,
+        _______, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    _______, _______, KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    _______,
+        _______, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    _______, _______, KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    ),
+
+    [_FN] = LAYOUT(
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,  KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
+        _______, KC_HOME, KC_UP,   KC_END,  _______, _______, _______, _______, _______, KC_HOME, KC_UP,   KC_END,  KC_PSCR, KC_PGUP,
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_INS,  KC_PGDN,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, RGB_MOD, _______, _______, _______, _______, _______, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU
+    ),
+
+    [_ADJUST] = LAYOUT(
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,  KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
+        _______, RGB_SAD, RGB_VAI, RGB_SAI, RESET,   _______, _______, _______, _______, KC_P7,   KC_P8,   KC_P9,   _______, _______,
+        _______, RGB_HUD, RGB_VAD, RGB_HUI, _______, _______, _______, _______, _______, KC_P4,   KC_P5,   KC_P6,   _______, _______,
+        _______, RGB_SPD, _______, RGB_SPI, _______, _______, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   _______, DVORAK,
+        _______, RGB_RMOD,_______, RGB_MOD, _______, _______, _______, _______, _______, KC_P0,   KC_PDOT, KC_NLCK, QWERTY, COLEMAK
+    ),
+};
+
+#ifdef ENCODER_ENABLE
+const uint16_t PROGMEM encoders[][NUMBER_OF_ENCODERS][2]  = {
+    [_QWERTY] = ENCODER_LAYOUT( \
+        KC_VOLU, KC_VOLD,
+        KC_VOLU, KC_VOLD,
+        KC_VOLU, KC_VOLD,
+        KC_VOLU, KC_VOLD
+    ),
+    [_COLEMAK] = ENCODER_LAYOUT( \
+        _______, _______,
+        _______, _______,
+        _______, _______,
+        _______, _______
+    ),
+    [_DVORAK] = ENCODER_LAYOUT( \
+        _______, _______,
+        _______, _______,
+        _______, _______,
+        _______, _______
+    ),
+    [_FN] = ENCODER_LAYOUT( \
+        _______, _______,
+        _______, _______,
+        _______, _______,
+        _______, _______
+    ),
+    [_ADJUST] = ENCODER_LAYOUT( \
+        _______, _______,
+        _______, _______,
+        _______, _______,
+        _______, _______
+    )
+};
+
+void encoder_update_user(uint8_t index, bool clockwise) {
+    if (!is_keyboard_master()) return;
+    uint8_t layer = biton32(layer_state);
+    uint16_t keycode = pgm_read_word(&encoders[layer][index][clockwise]);
+    while (keycode == KC_TRANSPARENT && layer > 0)
+    {
+        layer--;
+        if ((layer_state & (1 << layer)) != 0)
+            keycode = pgm_read_word(&encoders[layer][index][clockwise]);
+    }
+    if (keycode != KC_TRANSPARENT)
+        tap_code16(keycode);
+}
+#endif // ENCODER_ENABLE
+
+#if defined(RGB_MATRIX_ENABLE) || defined(RGBLIGHT_ENABLE)
+typedef void (*rgb_f)(void);
+const rgb_f rgb_functions[TOUCH_SEGMENTS] = { rgblight_step, rgblight_toggle, rgblight_step_reverse };
+
+void touch_encoder_tapped_user(uint8_t index, uint8_t section)
+{
+    xprintf("tap %d, %d\n", index, section);
+    (*rgb_functions[section])();
+}
+
+void touch_encoder_update_user(uint8_t index, bool clockwise)
+{
+    xprintf("slide %d, %d\n", index, clockwise);
+    // The rgb functions write to eeprom, which takes 3-4ms on avr.
+    if (clockwise)
+        rgblight_increase_hue();
+    else
+        rgblight_decrease_hue();
+}
+#endif
+
+#if defined(OLED_DRIVER_ENABLE)
+uint16_t ovalue = 0;
+uint32_t mcounter = 0;
+uint32_t mvalue = 0;
+uint16_t sec_timer = 1000;
+
+void do_counters(void) {
+    mcounter++;
+    uint16_t now = sync_timer_read();
+    if (timer_expired(now, sec_timer))
+    {
+        sec_timer += 1000;
+        ovalue++;
+        mvalue = (mvalue + mcounter) / 2;
+        mcounter = 0;
+    }
+}
+
+void matrix_scan_user(void) {
+    do_counters();
+}
+
+void matrix_slave_scan_user(void) {
+    do_counters();
+}
+
+void oled_task_user(void) {
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Layer: "), false);
+
+    switch (get_highest_layer(layer_state)) {
+        case _QWERTY:
+            oled_write_P(PSTR("Default\n"), false);
+            break;
+        case _FN:
+            oled_write_P(PSTR("FN\n"), false);
+            break;
+        case _ADJUST:
+            oled_write_P(PSTR("ADJ\n"), false);
+            break;
+        default:
+            // Or use the write_ln shortcut over adding '\n' to the end of your string
+            oled_write_ln_P(PSTR("Undefined"), false);
+    }
+
+    // Host Keyboard LED Status
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+    oled_write_ln_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+
+    static char buffer[6] = {0};
+    snprintf(buffer, sizeof(buffer), "%5d", ovalue);
+    oled_write_P(PSTR("\nTimer: "), false);
+    oled_write_ln_P(buffer, false);
+
+    snprintf(buffer, sizeof(buffer), "%5d", mvalue);
+    oled_write_P(PSTR("\nScans: "), false);
+    oled_write_ln_P(buffer, false);
+}
+#endif
---
+++ keyboards/rgbkb/mun/rev1/rev1.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "mun.h"
+
+// clang-format off
+#define LAYOUT( \
+    L00, L01, L02, L03, L04, L05, L06, R00, R01, R02, R03, R04, R05, R06, \
+    L10, L11, L12, L13, L14, L15, L16, R10, R11, R12, R13, R14, R15, R16, \
+    L20, L21, L22, L23, L24, L25, L26, R20, R21, R22, R23, R24, R25, R26, \
+    L30, L31, L32, L33, L34, L35, L36, R30, R31, R32, R33, R34, R35, R36, \
+    L40, L41, L42, L43, L44, L45, L46, R40, R41, R42, R43, R44, R45, R46  \
+) \
+{ \
+    /* Left Half */ \
+    { L00, L01, L02, L03, L04, L05, L06 }, \
+    { L10, L11, L12, L13, L14, L15, L16 }, \
+    { L20, L21, L22, L23, L24, L25, L26 }, \
+    { L30, L31, L32, L33, L34, L35, L36 }, \
+    { L40, L41, L42, L43, L44, L45, L46 }, \
+    /* Right Half */ \
+    { R06, R05, R04, R03, R02, R01, R00 }, \
+    { R16, R15, R14, R13, R12, R11, R10 }, \
+    { R26, R25, R24, R23, R22, R21, R20 }, \
+    { R36, R35, R34, R33, R32, R31, R30 }, \
+    { R46, R45, R44, R43, R42, R41, R40 }  \
+}
+
+#ifdef ENCODER_ENABLE
+#define NUMBER_OF_ENCODERS 4
+
+#define ENCODER_LAYOUT( \
+    E10, E11, \
+    E20, E21, \
+    E30, E31, \
+    E40, E41 \
+) \
+{ \
+    { E10, E11 }, \
+    { E20, E21 }, \
+    { E30, E31 }, \
+    { E40, E41 } \
+}
+#endif //ENCODER_ENABLE
+// clang-format on
