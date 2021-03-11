70f15ff93748f0f41c5f8de4670d2099204175b8
David <legonut3@gmail.com>
2021-03-11 15:19:37 -0500

    keymap updates

--- keyboards/rgbkb/sol3/keymaps/default/keymap.c
+++ keyboards/rgbkb/sol3/keymaps/default/keymap.c
@@ -15,16 +15,15 @@
      */
 #include QMK_KEYBOARD_H
 
-enum mun_layers {
+enum sol_layers {
     _QWERTY,
     _COLEMAK,
     _GAME,
-
     _FN,
     _ADJUST
 };
 
-enum mun_keycodes {
+enum sol_keycodes {
     QWERTY = SAFE_RANGE,
     RGBRST = SAFE_RANGE,
     RGB_MENU,
@@ -37,7 +36,7 @@ enum mun_keycodes {
 #define COLEMAK  DF(_COLEMAK)
 #define GAME     DF(_GAME)
 #define FN_CAPS  LT(_FN, KC_CAPS)
-#define RGB_ADJ  LT(_ADJUST, RGB_TOG)
+//#define RGB_ADJ  LT(_ADJUST, RGB_TOG)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -46,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                  KC_RBRC,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         FN_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LPRN,                  KC_RPRN,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LCBR,                  KC_RCBR,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
-        KC_LCTL,  KC_LGUI, KC_LALT, DM_PLY1, RGB_ADJ, KC_SPC,  KC_PGDN, KC_DEL, KC_ENT,  KC_PGUP,   KC_SPC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_RCTL
+        KC_LCTL,  KC_LGUI, KC_LALT, RGB_TOG, ADJUST, KC_SPC,  KC_PGDN, KC_DEL, KC_ENT,  KC_PGUP,   KC_SPC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_RCTL
     ),
 
     [_COLEMAK] = LAYOUT(
@@ -70,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, KC_HOME, KC_UP,   KC_END,  _______, _______, _______,                   _______, _______, KC_HOME, KC_UP,   KC_END,  KC_PSCR, KC_PGUP,
         _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______,                   _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_INS,  KC_PGDN,
         _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, _______,
-        _______, RGB_RMOD,_______, RGB_MOD, RGB_MOD, _______, _______, _______, _______, _______, _______, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU
+        _______, RGB_RMOD,_______, RGB_MOD, RGB_TOG, _______, _______, _______, _______, _______, _______, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU
     ),
 
     [_ADJUST] = LAYOUT(
@@ -78,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, RGB_SAD, RGB_VAI, RGB_SAI, RESET,   _______, _______,                   _______, _______, KC_P7,   KC_P8,   KC_P9,   _______, _______,
         _______, RGB_HUD, RGB_VAD, RGB_HUI, _______, _______, DM_REC1,                   _______, _______, KC_P4,   KC_P5,   KC_P6,   _______, _______,
         _______, RGB_SPD, _______, RGB_SPI, _______, _______, DM_RSTP,                   _______, _______, KC_P1,   KC_P2,   KC_P3,   _______, GAME,
-        _______, RGB_RMOD,_______, RGB_MOD, RGB_MOD, _______, _______, _______, _______, _______, _______, KC_P0,   KC_PDOT, KC_NLCK, QWERTY,  COLEMAK
+        _______, RGB_RMOD,_______, RGB_MOD, RGB_TOG, _______, _______, _______, _______, _______, _______, KC_P0,   KC_PDOT, KC_NLCK, QWERTY,  COLEMAK
     ),
 };
 
