691793252c4ee11e3fe673a3427494e50c5b0d43
Chad Braden <quesog@gmail.com>
2022-05-21 11:30:18 -0400

    Before I remembered I needed to swap right/left

--- keyboards/handwired/ergodox/config.h
+++ keyboards/handwired/ergodox/config.h
@@ -8,13 +8,20 @@
 #define MANUFACTURER QuesoG
 #define PRODUCT Handwired Ergodox
 
-#define MATRIX_ROWS 5
+// #define MASTER_RIGHT
+
+// Double the actual rows (5 for each half)
+#define MATRIX_ROWS 10
 #define MATRIX_COLS 9
 
 #define MATRIX_ROW_PINS { GP29, GP28, GP27, GP26, GP18 }
 #define MATRIX_COL_PINS { GP1, GP2, GP3, GP4, GP5, GP6, GP7, GP8, GP9 }
 
-// #define MATRIX_ROW_PINS_RIGHT { GP29, GP28, GP27, GP26, GP18 }
-// #define MATRIX_COL_PINS_RIGHT { GP1, GP2, GP3, GP4, GP5, GP6, GP7, GP8, GP9 }
+#define MATRIX_ROW_PINS_RIGHT { GP28, GP27, GP26, GP22, GP21 }
+#define MATRIX_COL_PINS_RIGHT { GP7, GP8, GP9, GP10, GP11, GP12, GP13, GP14, GP15 }
 
 #define DIODE_DIRECTION COL2ROW
+
+// Half duplex operations
+#define SERIAL_PIO_USE_PIO1 // Force the usage of PIO1 peripheral, by default the Serial implementation uses the PIO0 peripheral
+#define SERIAL_USART_TX_PIN GP19     // USART TX pin
--- keyboards/handwired/ergodox/keymaps/default/keymap.c
+++ keyboards/handwired/ergodox/keymaps/default/keymap.c
@@ -1,7 +1,20 @@
 #include QMK_KEYBOARD_H
 
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+[0] = LAYOUT_default(
+KC_GRV,  KC_1,        KC_2,  KC_3,  KC_4,  KC_5,  KC_NO,                                  KC_NO, KC_6, KC_7, KC_8,    KC_9,   KC_0,     KC_NO,
+KC_TAB,  KC_Q,        KC_W,  KC_E,  KC_R,  KC_T,  KC_NO,                                  KC_NO, KC_Y, KC_U, KC_I,    KC_O,   KC_P,     KC_NO,
+KC_ESC,  KC_A,        KC_S,  KC_D,  KC_F,  KC_G,  KC_NO, KC_NO, KC_NO,      KC_NO, KC_NO, KC_NO, KC_H, KC_J, KC_K,    KC_L,   KC_SCLN,  KC_NO,
+KC_LSFT, KC_Z,        KC_X,  KC_C,  KC_V,  KC_B,  KC_NO, KC_NO, KC_NO,      KC_NO, KC_NO, KC_NO, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLASH, KC_NO,
+KC_LCTL, KC_LEFT_GUI, KC_NO, KC_NO, KC_NO, KC_NO,               KC_NO,      KC_NO,               KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,    KC_NO
+)
+
+};
+
+
+/*
 [0] = LAYOUT_default(
 KC_GRV,  KC_1,        KC_2,  KC_3,  KC_4,  KC_5,  KC_NO,           \
 KC_TAB,  KC_Q,        KC_W,  KC_E,  KC_R,  KC_T,  KC_NO,           \
@@ -9,4 +22,4 @@ KC_ESC,  KC_A,        KC_S,  KC_D,  KC_F,  KC_G,  KC_NO, KC_NO, KC_NO, \
 KC_LSFT, KC_Z,        KC_X,  KC_C,  KC_V,  KC_B,  KC_NO, KC_NO, KC_NO, \
 KC_LCTL, KC_LEFT_GUI, KC_NO, KC_NO, KC_NO, KC_NO,           KC_NO  \
 )
-};
+*/
